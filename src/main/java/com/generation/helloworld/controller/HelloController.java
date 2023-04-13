b8409aa68d33fe9fee2b7882a2dd95473dd67412
Carolina Correa <carolina.unila@gmail.com>
2023-04-12 21:13:43 -0300

    Exercicio Spring

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -4,18 +4,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
-	
+
 	@GetMapping
 	public String hello() {
-		return "<b>Hello Carolina!</b>";
+		return "<b>Hello World!</b>";
+	}
+
+	@GetMapping("/bsm")
+	public String meuNome() {
+		return "<b>Mentalidades:</b> Orientação ao futuro, Responsabilidade pessoal, Mentalidade de Crescimento, "
+				+ "Persistência. <b>Competencias comportamentais</b>: Comunicação, Proatividade, Orientação ao detalhe,"
+				+ " Trabalho em equipe";
 	}
 	
-	@GetMapping("/nome")
-	public String meuNome() {
-		return "<b>Carolina</b>";
+	@GetMapping("/objetivo")
+	public String objetivo() {
+		return "Estudar e Aprender Spring e Banco de dados, aplica-los simultaneamente no projeto blog pessoal e no "
+				+ "projeto integrador";
 	}
 }
